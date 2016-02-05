69263a452c1c449a723655bcbb75c4388879b538
atlanticraphael <raphael.leva@atlanticsolutions.com.br>
2016-02-05 16:40:09 -0200

    Inclusões Classes

---
+++ store/grails-app/controllers/br/com/neuritos/controller/Header.groovy
@@ -0,0 +1,22 @@
+package br.com.neuritos.controller
+
+import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.support.RequestContextUtils
+
+class HeaderController {
+
+	transient springSecurityService
+	
+	def changeSystemLanguage(){
+		LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+		localeResolver.setLocale(request, response, new Locale(params.language));
+		
+		session["language"] = params.language
+		
+		redirect uri:'/'
+	}
+	
+	def homePage(){
+		render  view:'/index'
+	}
+}
---
+++ store/src/java/br/com/neuritos/controller/vo/DeleteVO.groovy
@@ -0,0 +1,8 @@
+package br.com.neuritos.controller.vo
+
+class DeleteVO {
+	
+	Long id
+	String description
+	String relationship
+}
